8905f999cfa990d032c695ebc37498fd67707149
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-07 22:26:28 +0530

    Add files via upload

---
+++ brakery--website/main.js
@@ -0,0 +1,29 @@
+
+
+// const nav = document.getElementById('list');
+// const ham = document.getElementById('ham');
+
+
+
+// ham.addEventListener('click',(e)=>{
+
+//     nav.classList.toggle('navlist-active');
+
+// });
+
+
+
+
+
+const nav = document.getElementById('list');
+const ham = document.getElementById('ham');
+
+
+
+ham.addEventListener('click',(e)=>{
+
+    nav.classList.toggle('navlist-active');
+
+});
+
+
